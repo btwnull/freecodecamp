8fe42c5faf55c25db31fa3851dabdd5961ccf0e7
Ivan Sobenin <dn.sobenin75@gmail.com>
2024-07-02 20:12:35 +0300

    step-3

---
+++ pyramid-generator/step-3.js
@@ -0,0 +1,10 @@
+// Step 3
+// Your character variable currently does not have a value. You can assign a value using the assignment operator =. For example:
+
+// Example Code
+// let hello = "Hello";
+// Assigning a value to a variable at the moment of its declaration is known as initialization.
+
+// Initialize your character variable by assigning it the value "Hello" during its declaration.
+
+let character = "Hello";
