6f58506a3577738543ac3fd144575056c0471ab2
Ivan Sobenin <dn.sobenin75@gmail.com>
2024-07-14 20:32:54 +0300

    step-101

---
+++ pyramid-generator/step-101.js
@@ -0,0 +1,5 @@
+// Your iteration statement is also going to be different. Instead of adding 1 to i with each loop, you need to subtract 1.
+
+// Like you did earlier with i = i + 1, update your iteration statement to give i the value of subtracting 1 from itself.
+
+for (let i = count; i > 0; i = i - 1) {}
