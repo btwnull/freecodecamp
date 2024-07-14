37cc44b3e342cdcd78df5c9b3333b90592114374
Ivan Sobenin <dn.sobenin75@gmail.com>
2024-07-14 20:36:13 +0300

    step-103

---
+++ pyramid-generator/step-103.js
@@ -0,0 +1,7 @@
+// Just like addition, there are different operators you can use for subtraction. The subtraction assignment operator -= subtracts the given value from the current variable value, then assigns the result back to the variable.
+
+// Replace your iterator statement with the correct statement using the subtraction assignment operator
+
+for (let i = count; i > 0; i -= 1) {
+  rows.push(padRow(i, count));
+}
