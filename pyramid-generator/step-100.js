e119f2180eb6b6fdf265b873217c07fe9eaf5c6d
Ivan Sobenin <dn.sobenin75@gmail.com>
2024-07-14 20:30:49 +0300

    step-100

---
+++ pyramid-generator/step-100.js
@@ -0,0 +1,6 @@
+// Step 100
+// Because you are going to loop in the opposite direction, your loop needs to run while i is greater than 0. You can use the greater than operator > for this.
+
+// Set your loop's condition to run when i is greater than 0.
+
+for (let i = count; i > 0; false) {}
