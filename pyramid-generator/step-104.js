2d93e7696dd395f57410254e94b6fc8a5d1ada8d
Ivan Sobenin <dn.sobenin75@gmail.com>
2024-07-14 20:38:57 +0300

    step-104

---
+++ pyramid-generator/step-104.js
@@ -0,0 +1,7 @@
+// Because you are only subtracting one from i, you can use the decrement operator --.
+
+// Replace your subtraction assignment with the decrement operator.
+
+for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+}
