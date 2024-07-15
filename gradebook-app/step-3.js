6278a8592b55e59068e94e9769b38ce7db42d0c4
Ivan Sobenin <dn.sobenin75@gmail.com>
2024-07-15 19:33:13 +0300

    step-3

---
+++ gradebook-app/step-3.js
@@ -0,0 +1,9 @@
+// The teacher is really happy with the program you have created so far. But now they want to have an easy way to check if a student has a passing grade. A passing grade is anything that is not an "F".
+
+// Complete the function hasPassingGrade that takes a student score as a parameter. Your function should return true if the student has a passing grade and false if they do not.
+
+// Tips
+
+// Use the getGrade function to get the student's grade. Then check if the grade is passing or not.
+
+return score > 59;
